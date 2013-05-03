1df9e64c4a4699e8f35c2ab335525624a1ea0fa9
tasuten <heliopause64@gmail.com>
2013-05-04 04:08:11 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# 手動管理
+DOTFILES=(.vimrc .zshrc .zprofile .tmux.conf .vim/template .vim/snippets .vim/ftplugin .vim/after/ftplugin )
+
+# このディレクトリを原点に環境を構築する
+# ので、スクリプトのテストする時はテスト用のディレクトリに変えて
+ROOT_DIR=$HOME/
+
+for file in ${DOTFILES[@]}; do
+  # ディレクトリへのシンボリックリンクでその親ディレクトリが無いとエラーるので
+  if [ ! -e $ROOT_DIR/.vim ]; then
+    mkdir $ROOT_DIR/.vim
+  fi
+  if [ ! -e $ROOT_DIR/.vim/after ]; then
+    mkdir $ROOT_DIR/.vim/after
+  fi
+
+  ln -s $HOME/dotfiles/$file $ROOT_DIR/$file
+
+done
+
