a38cda1e5291e9cbf25847b7ca770bb94ed263c6
tasuten <heliopause64@gmail.com>
2016-01-28 05:08:36 +0900

    Renew setup.sh, smarter!

--- setup.sh
+++ setup.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
 # 手動管理
-DOTFILES=(.vimrc .zshrc .zprofile .tmux.conf .gitconfig .rbenv/default-gems .vim/template .vim/snippets .vim/ftplugin .vim/after/ftplugin)
+# ディレクトリ名でも最期に/を入れない
+DOTFILES="
+.vimrc
+.zshrc
+.zprofile
+.tmux.conf
+.gitconfig
+.rbenv/default-gems
+.vim/template
+.vim/snippets
+.vim/ftplugin
+.vim/after/ftplugin
+"
 
-# このディレクトリを原点に環境を構築する
-# ので、スクリプトのテストする時はテスト用のディレクトリに変えて
-ROOT_DIR=$HOME/
+# 環境構築先のルートディレクトリ
+# 普通は~/だけどテスト時とかは適宜変更して
+ROOT_DIR=$HOME
 
-# ディレクトリのシンボリックリンクでその親ディレクトリが無いとエラーるので
-if [ ! -e $ROOT_DIR/.vim ]; then
-  mkdir $ROOT_DIR/.vim
-fi
-if [ ! -e $ROOT_DIR/.vim/after ]; then
-  mkdir $ROOT_DIR/.vim/after
-fi
+# dotfilesディレクトリ
+# 例えば~/dotfiles
+BASE_DIR=$(cd "$(dirname "$0")" || return 1;pwd)
 
-if [ ! -e $ROOT_DIR/.rbenv ]; then
-  mkdir $ROOT_DIR/.rbenv
-fi
+for file in $DOTFILES; do
+  # 存在しないディレクトリの中にsymlinkを作ろうとすると怒られるので
+  parent_dir=$(dirname "$ROOT_DIR/$file")
+  if [ ! -d "$parent_dir" ]; then
+    mkdir -p "$parent_dir"
+  fi
 
-for file in ${DOTFILES[@]}; do
-  ln -s $HOME/dotfiles/$file $ROOT_DIR/$file
+  # 古いリンクがあるなら削除
+  if [ -L "$ROOT_DIR/$file" ]; then
+    unlink "$ROOT_DIR/$file"
+  fi
+
+  ln -s "$BASE_DIR/$file" "$ROOT_DIR/$file"
 done
+
+echo "Done Successfully."
 
