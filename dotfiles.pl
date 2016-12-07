91759f45411aed8e05fd275e48abc1e9d6900841
tasuten <heliopause64@gmail.com>
2016-12-07 20:42:55 +0900

    Refactoring

--- dotfiles.pl
+++ dotfiles.pl
@@ -1,57 +1,61 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use feature ":5.10";
+use feature ':5.10';
 
-use File::Basename;
-use File::Path;
-use Cwd;
+use File::Basename qw/dirname/;
+use File::Path qw/mkpath/;
+use Cwd qw/getcwd/;
 
 use Data::Dumper;
 
 # main
-our %config = parse_config("./dotfiles_config");
-our %link_table = generate_table();
-# print Dumper %link_table;
-if ($#ARGV == -1 || $#ARGV > 2) {
+if ($#ARGV == -1 or $#ARGV > 2) {
     usage() and exit 1;
 }
-our $dry = undef;
-if ($#ARGV == 1 && $ARGV[1] eq "--dry") {
-    $dry = 'true';
+
+my %config = parse_config('dotfiles_config');
+my %link_table = generate_table(\%config);
+
+my $dry_run = undef;
+if ($#ARGV == 1 and $ARGV[1] eq '--dry') {
+    $dry_run = 'true';
 }
 
-if ($ARGV[0] eq "link") {
-    links();
-} elsif ($ARGV[0] eq "unlink") {
-    unlinks();
-} elsif ($ARGV[0] eq "refresh") {
-    unlinks();
-    links();
+# Subcommand
+if ($ARGV[0] eq 'link') {
+    links(\%link_table, $dry_run);
+} elsif ($ARGV[0] eq 'unlink') {
+    unlinks(\%link_table, $dry_run);
+} elsif ($ARGV[0] eq 'refresh') {
+    unlinks(\%link_table, $dry_run);
+    links(\%link_table, $dry_run);
 } else {
     usage() and exit 1;
 }
 
-
 # main end
 
 sub parse_config {
-    my %config;
     my ($config_file) = @_;
-    open CONFIG, $config_file || die "Can't open $config_file: $!";
+    my %config;
+    open my $config_fh, '<', $config_file
+        or die "Can't open $config_file: $!";
 
-    while (my $line = readline CONFIG) {
+    while (my $line = readline $config_fh) {
         chomp $line;
+
+        # comment or empty line
         if ($line =~ /^(?:\s*#.*)|(?:\s+)$/) {
             next;
-        } elsif ($line =~ /^\[(.+)\]/) {
+        } elsif ($line =~ /^\[(.+)\]/) { # [Section]
             my $section = lc $1;
 
-            if ($section eq "general") {
-                %{$config{$section}} = parse_hash(*CONFIG);
+            if ($section eq 'general') {
+                %{$config{$section}} = parse_hash(*$config_fh);
             } else {
-                my @result = parse_list(*CONFIG);
-                if ($section eq "directory") {
+                my @result = parse_list(*$config_fh);
+                if ($section eq 'directory') {
                     @result = map { normalize_dir($_) } @result;
                 }
                 @{$config{$section}} = @result;
@@ -61,7 +65,7 @@ sub parse_config {
         }
     }
 
-    close CONFIG;
+    close $config_fh;
     return %config;
 }
 
@@ -70,7 +74,7 @@ sub parse_hash {
     my %kvs;
     while (my $line = readline $fh) {
         chomp $line;
-        if ($line eq "") {
+        if ($line eq '') { # section end is empty line
             return %kvs;
         } elsif ($line =~ /^(?:\s*#.*)$/) {
             next;
@@ -86,7 +90,7 @@ sub parse_list {
     my @elms;
     while (my $line = readline $fh) {
         chomp $line;
-        if ($line eq "") {
+        if ($line eq '') { # section end is empty line
             return @elms;
         } elsif ($line =~ /^(?:\s*#.*)$/) {
             next;
@@ -96,38 +100,41 @@ sub parse_list {
     }
 }
 
+# Add tail slash and expand $HOME
 sub normalize_dir {
     my ($dir) = @_;
-    if (substr($dir, -1) ne "/") {
-        $dir = "$dir/";
+    if (substr($dir, -1) ne '/') {
+        $dir = $dir . '/';
     }
 
-    my $homedir = $ENV{"HOME"};
+    # FIXME: better way
+    my $homedir = $ENV{HOME};
     $dir =~ s/\$HOME/$homedir/g;
     return $dir;
 }
 
 
 sub generate_table {
+    my (@config) = @_;
     my %result;
 
     # directories
     for my $dir (@{$config{directory}}){
-        my $entity = getcwd . "/" .  $dir;
-        my $root = normalize_dir($config{"general"}{"root"});
+        my $entity = getcwd . '/' .  $dir;
+        my $root = normalize_dir($config{general}{root});
         my $symlink = $root . $dir;
-        $symlink =~ s/\/+$//g;
+        $symlink =~ s/\/+$//g; # symlink name doesn't finish with slash(es)
         $result{$entity} = $symlink;
     }
 
     # files
     my @files = split /\n/, `git ls-files`;
     for my $f (@files){
-        if (is_ignore($f) || in_directory($f)) {
+        if (is_ignore($f) or in_directory($f)) {
             next;
         } else {
-            my $entity = getcwd . "/" . $f;
-            my $root = normalize_dir($config{"general"}{"root"});
+            my $entity = getcwd . '/' . $f;
+            my $root = normalize_dir($config{general}{root});
             my $symlink = $root . $f;
             $result{$entity} = $symlink;
         }
@@ -139,6 +146,7 @@ sub generate_table {
 sub is_ignore {
     my ($f) = @_;
     for my $ignore (@{$config{ignore}}){
+        # FIXME: smarter way
         if ($f =~ /^$ignore/) {
             return 'true';
         }
@@ -150,6 +158,7 @@ sub is_ignore {
 sub in_directory {
     my ($f) = @_;
     for my $dir (@{$config{directory}}){
+        # FIXME: smarter way
         if ($f =~ /^$dir/) {
             return 'true';
         }
@@ -158,37 +167,46 @@ sub in_directory {
 }
 
 sub usage {
-    print "$0 (link|unlink|refresh) [--dry]\n"
+    warn "$0 (link|unlink|refresh) [--dry]\n"
 }
 
 sub links {
+    my ($link_table, $dry_run) = @_;
     while (my ($entity, $symlink) = each(%link_table)) {
-        my $parent_dir = dirname($symlink);
-        if (!-d $parent_dir ) {
-            if ($dry) {
-                print "mkdir $parent_dir";
+
+        # if parent directory doesn't exist
+        my $parent_dir = dirname $symlink;
+        if (!-d $parent_dir) {
+            if ($dry_run) {
+                print "mkdir $parent_dir\n";
             } else {
-            mkpath($parent_dir);
+                mkpath $parent_dir;
+            }
         }
-        }
-        if ($dry) {
+
+        if ($dry_run) {
             print "symlink $entity -> $symlink\n"
         } else {
-        symlink $entity,  $symlink;
-    }
+            symlink $entity,  $symlink;
+        }
     }
 }
 
 sub unlinks {
+    my ($link_table, $dry_run) = @_;
+
     while (my ($entity, $symlink) = each(%link_table)) {
-        if (-l $symlink ) {
-            # unlinkは通常ファイルも消し飛ばすので注意
-            if ($dry) {
+
+        if (-l $symlink ) { # if symbolic link
+            # unlink removes normal files too!!!!
+            if ($dry_run) {
                 print "unlink $symlink\n"
             } else {
-            unlink $symlink;
+                unlink $symlink;
+            }
         }
-        }
+
     }
+
 }
 
