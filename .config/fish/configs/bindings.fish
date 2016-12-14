c88c183b8e8e2ba410eac5512a7957aa879eb23d
tasuten <heliopause64@gmail.com>
2016-12-15 02:20:56 +0900

    Fix a comment

--- .config/fish/configs/bindings.fish
+++ .config/fish/configs/bindings.fish
@@ -3,7 +3,8 @@
 # Ctrl-F,  Ctrl-Bでワード単位移動
 bind \cf forward-word
 bind \cb backward-word
-# Ctrl-D, Ctrl-Wでforward方向にワード削除
-bind \cd kill-word
+# Ctrl-D, Ctrl-Wでワード削除
 bind \cw backward-kill-word
+# FIXME:何故か効いてない↓
+bind \cd kill-word
 
