c8274e1f8e1f85735fb9ac926a9f93f308e7f61c
tasuten <heliopause64@gmail.com>
2016-12-15 03:39:50 +0900

    Keybinding needs to be in fish_user_key_binding()
    https://github.com/fish-shell/fish-shell/issues/1495

--- .config/fish/configs/bindings.fish
+++ .config/fish/configs/bindings.fish
@@ -1,10 +1,11 @@
 # fishのキーバインド
 
-# Ctrl-F,  Ctrl-Bでワード単位移動
-bind \cf forward-word
-bind \cb backward-word
-# Ctrl-D, Ctrl-Wでワード削除
-bind \cw backward-kill-word
-# FIXME:何故か効いてない↓
-bind \cd kill-word
+function fish_user_key_bindings
+  # Ctrl-F,  Ctrl-Bでワード単位移動
+  bind \cf forward-word
+  bind \cb backward-word
+  # Ctrl-D, Ctrl-Wでワード削除
+  bind \cw backward-kill-word
+  bind \cd kill-word
+end
 
