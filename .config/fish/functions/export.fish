bc1841e3f04faa51b398dc2f0ed43e62d92d531d
tasuten <heliopause64@gmail.com>
2016-12-15 00:33:14 +0900

    Immigrant exporting values

--- .config/fish/functions/export.fish
+++ .config/fish/functions/export.fish
@@ -1,11 +1,13 @@
 # exportする環境変数
 
+# 基本的なこと
+set -x LANG ja_JP.UTF-8
+set -x EDITOR /Applications/MacVim.app/Contents/MacOS/Vim
+
+# パスを通す
 # Homebrew
 set -Ux PATH /usr/local/bin $PATH
 set -Ux PATH /usr/local/sbin $PATH
-
-# w3m
-set -x WWW_HOME 'www.google.co.jp'
 
 # rlwrap
 set -x RLWRAP_HOME $HOME/.rlwrap/
@@ -20,4 +22,13 @@ set -x BSTINPUTS $BSTINPUTS $HOME/.TeX/bibtex/bst
 
 # ~/bin/
 set -x PATH $HOME/bin $PATH
+
+# 各ソフトウェアの設定
+# lessが色付きテキストを表示出来るようにする
+set -x LESS '-R'
+# grepでヒット部分をシアンで
+set -x GREP_OPTIONS '--color=auto'
+set -x GREP_COLOR '00;36'
+# w3m
+set -x WWW_HOME 'www.google.co.jp'
 
