c0662bbe6671b6379a787068563b4769bd6a250a
tasuten <heliopause64@gmail.com>
2016-12-16 05:23:47 +0900

    Set GNU style LS_COLORS

--- .config/fish/configs/export.fish
+++ .config/fish/configs/export.fish
@@ -33,6 +33,7 @@ set -x PATH $HOME/bin $PATH
 # 各ソフトウェアの設定
 # lsの色
 set -x LSCOLORS 'gxhxcxdxbxaeagabafacad'
+set -x LS_COLORS 'di=36:ln=37:so=32:pi=33:ex=31:bd=30;44:cd=30;46:su=30;41:sg=30;45:tw=30;42:ow=30;43'
 # lessが色付きテキストを表示出来るようにする
 set -x LESS '-R'
 # grepでヒット部分をシアンで
