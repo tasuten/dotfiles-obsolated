4c8115059519282ee2c5c1a7611229c46e9443ce
tasuten <heliopause64@gmail.com>
2016-12-15 17:56:46 +0900

    Set shortcut aliases, instead of zsh-global-alias

--- .config/fish/configs/bindings.fish
+++ .config/fish/configs/bindings.fish
@@ -8,6 +8,16 @@ function fish_user_key_bindings
   bind \cw backward-kill-word
   bind \cd kill-word
 
+  # zshのglobal alias代わり
+  # 指定のキーで指定の文字列を挿入
+  # prefixに\cs
+  # 一部直感的でないのは他のkeybindとの兼ね合いなど
+  bind \cs\cl "commandline -rt '| less'"
+  bind \cs\cj "commandline -rt '| head'"
+  bind \cs\ck "commandline -rt '| tail'"
+  bind \cs\cg "commandline -rt '| grep'"
+  bind \cs\cy "commandline -rt '| pbcopy'"
+
   # fzf
   # prefixに\cx
   bind \cx\cd __fzf_z # z
