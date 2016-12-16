8c17dd1cad58d8466b22a25261eec867774d015d
tasuten <heliopause64@gmail.com>
2016-12-16 15:29:12 +0900

    fzf sources accept single values

--- .config/fish/configs/fzf.fish
+++ .config/fish/configs/fzf.fish
@@ -4,7 +4,7 @@ function __fzf_z
     or set -l FZF_Z_COMMAND "
   __z -l 2>&1 | sed -E 's/^[0-9]+(.[0-9]+)? +//g'"
 
-    fish -c "$FZF_Z_COMMAND" | __fzfcmd -m $FZF_Z_OPTS | read -la select
+    fish -c "$FZF_Z_COMMAND" | __fzfcmd $FZF_Z_OPTS | read -la select
     if test ! (count $select) -eq 0
         cd "$select"
     end
@@ -17,7 +17,7 @@ function __fzf_git_tracking
     set -q FZF_GIT_TRACKING
     or set -l FZF_GIT_TRACKING_COMMAND "git ls-files --full-name $__GIT_ROOT_PATH"
 
-    fish -c "$FZF_GIT_TRACKING_COMMAND" | __fzfcmd -m $FZF_GIT_TRACKING_OPTS | __fzfescape | read -la selects
+    fish -c "$FZF_GIT_TRACKING_COMMAND" | __fzfcmd $FZF_GIT_TRACKING_OPTS | __fzfescape | read -la selects
     if test ! (count $selects) -eq 0
         vim "$__GIT_ROOT_PATH$selects"
     end
@@ -30,7 +30,7 @@ function __fzf_git_branch
     set -q FZF_GIT_BRANCH
     or set -l FZF_GIT_BRANCH_COMMAND 'git branch -vv | grep -v "^\*"'
 
-    fish -c "$FZF_GIT_BRANCH_COMMAND" | __fzfcmd -m $FZF_GIT_TRACKING_OPTS | read -la select
+    fish -c "$FZF_GIT_BRANCH_COMMAND" | __fzfcmd $FZF_GIT_TRACKING_OPTS | read -la select
     if test ! (count $select) -eq 0
         set -l branch  (echo "$select" | cut -d \  -f 1)
         git checkout "$branch"
