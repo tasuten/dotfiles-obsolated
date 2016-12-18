e0d8b5621d6347179f3bf9fef983b02cbaea7bfa
tasuten <heliopause64@gmail.com>
2016-12-18 11:55:21 +0900

    Add fzf-history keybind
    NOTE:
    __fzf_find_and_execute is execute immediately
    But __fzf_find_and_execute only puts the command
    in command line, so I can edit the command

--- .config/fish/configs/bindings.fish
+++ .config/fish/configs/bindings.fish
@@ -21,6 +21,7 @@ function fish_user_key_bindings
     # fzf
     # prefixに\cx
     bind \cr __fzf_find_and_execute
+    bind \cx\ch __fzf_reverse_isearch
     bind \cx\cd __fzf_z # z
     bind \cx\cf __fzf_git_tracking
     bind \cx\cb __fzf_git_branch
