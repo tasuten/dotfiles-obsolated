ad7f1ba8dd5fa724b6d2360acd25b0cb2c5367dc
tasuten <heliopause64@gmail.com>
2017-05-11 22:28:57 +0900

    dotfile definition and sample use(tmux)

--- cookbooks/common/default.rb
+++ cookbooks/common/default.rb
@@ -1,6 +1,12 @@
 # common settings that is not related platform
-# e.g.)
-#
-# define :new_command, arg1: nil, args2: nil do
-#   # something to do
-# end
+
+define :dotfile, add_dot: false do
+  entity = params[:name]
+  symlink = params[:add_dot] ? ".#{entity}" : entity 
+
+  link_root = ENV['HOME']
+
+  link File.join(link_root, symlink) do
+    to File.expand_path("../../dotfiles/#{entity}", __FILE__)
+  end
+end
--- roles/common/default.rb
+++ roles/common/default.rb
@@ -1,2 +1,4 @@
 # common settings that is not related platform
 include_cookbook 'common'
+
+include_cookbook 'tmux'
