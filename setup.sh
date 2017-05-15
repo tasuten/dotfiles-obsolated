8a61dfb66bf453b89fc869f2e3af315ccfb279fc
tasuten <heliopause64@gmail.com>
2017-05-16 03:43:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ ln -sf `pwd`/.config/git/config ~/.config/git/config
 ln -sf `pwd`/.vimrc ~/.vimrc
 ln -sf `pwd`/.vim/ftplugin ~/.ftplugin
 ln -sf `pwd`/.tmux.conf ~/.tmux.conf
+ln -sf `pwd`/.zprofile ~/.zprofile
+ln -sf `pwd`/.zshrc ~/.zshrc
