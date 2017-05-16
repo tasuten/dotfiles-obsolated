4634c3200bf3614ca2bd709e0c121daa453a2313
tasuten <heliopause64@gmail.com>
2017-05-16 22:01:08 +0900

    Remove setup script

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-ln -sf `pwd`/.config/git/config ~/.config/git/config
-ln -sf `pwd`/.vimrc ~/.vimrc
-ln -sf `pwd`/.vim/ftplugin ~/.ftplugin
-ln -sf `pwd`/.tmux.conf ~/.tmux.conf
-ln -sf `pwd`/.zprofile ~/.zprofile
-ln -sf `pwd`/.zshrc ~/.zshrc
