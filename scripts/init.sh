94d1b34eaf99f913afe4619dd9b0422e442c7531
tasuten <heliopause64@gmail.com>
2017-05-23 18:12:58 +0900

    symlink()

--- scripts/init.sh
+++ scripts/init.sh
@@ -22,8 +22,28 @@ mkdir_p() {
   fi
 }
 
+symlink() {
+  local entity link
+  entity=$1
+  link=$2
+
+  if [[ -L $link ]]; then
+    unlink "$link"
+    ln -s "$entity" "$link"
+  elif [[ -e $link ]]; then
+    die "A file already exists at $link"
+  else
+    local parent
+    parent=$(dirname "$link")
+    if [[ ! -d "$parent" ]]; then
+      mkdir_p "$parent"
+    fi
+    ln -s "$entity" "$link"
+  fi
+}
+
 
 source_config
 
-unset -f die mkdir_p
+unset -f die mkdir_p symlink
 # unset -v var
