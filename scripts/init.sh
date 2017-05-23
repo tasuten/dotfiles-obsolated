99f2bd50927cdfada09010d71726c1a28364e3a3
tasuten <heliopause64@gmail.com>
2017-05-23 17:16:37 +0900

    typo

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# when any command returns 0, exit this script
+# when any command returns non-0, exit this script
 set -e
 
 source "$(cd $(dirname $0) && pwd)"/configs.sh
+
+
 
 # unset -f func
 # unset -v var
