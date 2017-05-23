8237e9148d94798683ce4a8f1f4a5974fb75a994
tasuten <heliopause64@gmail.com>
2017-05-23 17:13:21 +0900

    Purge config file

---
+++ scripts/configs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Set config vars
+
--- scripts/init.sh
+++ scripts/init.sh
@@ -3,5 +3,7 @@
 # when any command returns 0, exit this script
 set -e
 
+source "$(cd $(dirname $0) && pwd)"/configs.sh
+
 # unset -f func
 # unset -v var
