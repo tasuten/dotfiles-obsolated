674f2ad8d7148790a3923512e537b6c53eb94d34
tasuten <heliopause64@gmail.com>
2017-05-26 01:49:03 +0900

    tweak

--- init.sh
+++ init.sh
@@ -8,7 +8,11 @@ die() {
   exit 1
 }
 
+# Idempotent mkdir -p
 mkdir_p() {
+  if [[ $# -ne 1 ]]; then
+    die "mkdir_p dir_name"
+  fi
   local target
   target=$1
   if [[ ! -d $target ]]; then
@@ -16,7 +20,12 @@ mkdir_p() {
   fi
 }
 
+# Idempotent ln -s
 symlink() {
+  if [[ $# -ne 2 ]]; then
+    die "symlink entity_path link_path"
+  fi
+
   local entity link
   entity=$1
   link=$2
@@ -36,13 +45,18 @@ symlink() {
   fi
 }
 
+# it links a file
 link () {
+  if [[ $# -ne 1 ]]; then
+    die "link path_from_dotfiles_root"
+  fi
   local dotfiles root
   dotfiles="$HOME/dotfiles"
   root="$HOME"
   symlink "$dotfiles/$1" "$root/$1"
 }
 
+# it links a directory
 link_d () {
   link "${1/%\/}"
 }
@@ -51,12 +65,12 @@ dir () {
   mkdir_p "$HOME/$1"
 }
 
-link ".config/git/config"
+link   ".config/git/config"
 link_d ".config/zsh/"
-link ".tmux.conf"
-link ".vimrc"
-link_d ".vim/ftplugin"
-link ".zshenv"
+link   ".tmux.conf"
+link   ".vimrc"
+link_d ".vim/ftplugin/"
+link   ".zshenv"
 
 dir ".vim/tmp/swap"
 dir ".vim/tmp/backup"
@@ -73,4 +87,4 @@ dir ".local/share/rustup"
 dir ".local/share/cargo"
 dir ".local/share/go"
 
-unset -f die mkdir_p symlink link dir
+unset -f die mkdir_p symlink link link_d dir
